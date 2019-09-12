4d40561270d9eaede14040128a97293033f0b2f8
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-09-11 20:59:01 -0500

    basic control structure in place

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.omdb = {
+  secret: process.env.OMDB_KEY
+}
+
+exports.bandisintown = {
+  id: process.env.BANDISINTOWN_KEY
+}
--- liri.js
+++ liri.js
@@ -1,5 +1,52 @@
 require("dotenv").config();
 
-const spotify = new Spotify(keys.spotify);
+
+const keys = require("./keys.js");
+const axios = require("axios")
+
+const 
+// const spotify = new Spotify(keys.spotify);
 
 const option = process.argv[2];
+
+const term = process.argv.slice(3).join("+");
+
+// console.log(
+
+// `
+// --------------
+
+// testing template literal
+
+// --------------
+    
+// `
+// )
+
+
+
+switch (option) {
+    case 'concert-this':
+        const URL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
+
+
+
+        break;
+
+
+    case 'spotify-this-song':
+
+        break;
+
+
+    case 'movie-this':
+
+        break;
+
+
+    case 'do-what-it-says':
+
+
+        break;
+
+}
